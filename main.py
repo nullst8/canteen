de5357f91f075a4ef029f69a6b4b708d84f9771e
nullst8 <nullst8@proton.me>
2022-11-13 16:14:47 +0530

    created a fn to check if db exists. if not, created!

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from pyzbar import pyzbar
+import pymysql as pm
 import pyfiglet
 from cv2 import cv2
 
 pyfiglet.print_figlet("canteen app")
+
+conn = pm.connect(host="localhost",
+                  user="nullst8",
+                  password="mann@2312",
+                  db="canteen")
+cur = conn.cursor()
 
 
 def ReadBarcode(image):
@@ -16,15 +23,28 @@ def ReadBarcode(image):
 
         if barcode.data != "":
             if barcode.data.decode('ascii')[:4] == "N44-":
-                print("your admission number is:{}".format(
-                    barcode.data.decode('ascii')[4:]))
+                admNo = barcode.data.decode('ascii')[4:]
+                return admNo
             else:
                 print("barcode not recognized!")
         else:
             print("err reading barcode!!")
 
 
-# selImg = input(r"enter img path: ")
-# ReadBarcode(selImg)
-ReadBarcode("barcode.jpeg")
-# ReadBarcode("not.png")
+def CheckUserExists(admNo):
+    exists = cur.execute("show tables like 'a{}'".format(admNo))
+    return True if exists == 1 else False
+
+
+def CreateEntry(admNo):
+    dbInitQuery = "sno int not null auto_increment primary key, item varchar(30) not null,price int not null,debt int"
+    if CheckUserExists(admNo):
+        pass
+    else:
+        cur.execute("create table a{}({})".format(admNo, dbInitQuery))
+
+
+# a = ReadBarcode("barcode.jpeg")
+# print(a)
+
+CreateEntry(30120)
