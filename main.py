2e04850997ec48bcb6e33d2843f0b7f9baf0e00f
nullst8 <nullst8@proton.me>
2022-11-15 23:47:49 +0530

    created a fn that asks for info regarding user entry

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymysql as pm
 import pyfiglet
 from cv2 import cv2
 
-pyfiglet.print_figlet("canteen app")
+pyfiglet.print_figlet("Canteen App")
 
 conn = pm.connect(host="localhost",
                   user="nullst8",
@@ -17,18 +17,17 @@ def ReadBarcode(image):
     barcodes = pyzbar.decode(img)
 
     if not barcodes:
-        print("err detecting barcode")
+        print("Error Detecting Barcode")
 
     for barcode in barcodes:
-
         if barcode.data != "":
             if barcode.data.decode('ascii')[:4] == "N44-":
                 admNo = barcode.data.decode('ascii')[4:]
                 return admNo
             else:
-                print("barcode not recognized!")
+                print("Barcode Not Recognized!")
         else:
-            print("err reading barcode!!")
+            print("Error Reading Barcode!!")
 
 
 def CheckUserExists(admNo):
@@ -36,15 +35,41 @@ def CheckUserExists(admNo):
     return True if exists == 1 else False
 
 
+def AskInfo(admNo):
+    inDebt = False
+    nos = int(input("Enter Number of Items to Buy: "))
+    for i in range(nos):
+        item = input("What Item are You Buying: ")
+        amt = input("Enter Amount: ")
+        pn = input("Would you like to Pay for it now?(y/n): ")
+        inDebt = False if pn[0] == 'y' else True
+
+        if inDebt:
+            cur.execute("insert into a{} (item,price) values ('{}',{})".format(
+                admNo, item, amt))
+            cur.execute("update a{} set debt = debt + {}".format(admNo, amt))
+            cur.execute("select debt from a{}".format(admNo))
+            q = cur.fetchall()
+
+            for i in q:
+                print(i)
+
+            print("done1")
+        else:
+            cur.execute("insert into a{} (item,price) values ('{}',{})".format(
+                admNo, item, amt))
+            print("done2")
+
+
 def CreateEntry(admNo):
     dbInitQuery = "sno int not null auto_increment primary key, item varchar(30) not null,price int not null,debt int"
+
     if CheckUserExists(admNo):
-        pass
+        AskInfo(admNo)
     else:
         cur.execute("create table a{}({})".format(admNo, dbInitQuery))
+        AskInfo(admNo)
 
 
-# a = ReadBarcode("barcode.jpeg")
-# print(a)
-
-CreateEntry(30120)
+a = ReadBarcode("barcode.jpeg")
+CreateEntry(a)
